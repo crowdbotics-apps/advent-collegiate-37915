85110acb34cc71c77db87f5dd0054d8263e6d0a8
Crowdbotics <team@crowdbotics.com>
2022-12-15 22:19:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "advent_collegiate_37915",
+  url: "https://advent_collegiate_37915.botics.co",
+  api: "https://advent_collegiate_37915.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
